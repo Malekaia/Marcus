2ff91145edd537f6d92400ba8e625efdb642ee27
Malekai <>
2022-09-15 06:19:52 +0100

    support HTML escapes

---
+++ src/parser/escape.rs
@@ -0,0 +1,19 @@
+use crate::regex::RE;
+use html_escape;
+use regex::Regex;
+use std::borrow::Cow;
+
+pub fn default(html: &mut String) {
+  // Create the escape regex
+  let re: Regex = Regex::new(RE::ESCAPE).unwrap();
+  // Check for escaped characters
+  if re.is_match(html) {
+    // Iter all captures
+    for capture in re.captures_iter(html.clone().as_ref()) {
+      // HTML encode the escaped character (or return the raw character if not required)
+      let replacement: Cow<str> = html_escape::encode_safe(&capture[1]);
+      // Replace the raw escape with the encoded HTML
+      *html = html.replace(&capture[0], &replacement);
+    }
+  }
+}
