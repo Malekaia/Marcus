e4dd0d7ce4c94148b0137c66788795a97b37bfe9
Malekai <>
2022-09-29 19:17:58 +0100

    improve documentation & error handling

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,28 @@ mod parser;
 mod regex;
 use std::fs;
 
-// File read util
 fn read_file(file_path: &str) -> String {
-  fs::read_to_string(file_path)
-     .expect("Should have been able to read the file")
+  // Read the file
+  match fs::read_to_string(file_path) {
+    // Return the file content
+    Ok(file_content) => file_content,
+    // Output error
+    Err(error_message) => panic!("Marcus: ReadError: failed to read {}\n\n{}", &file_path, &error_message)
+  }
 }
 
 fn main() {
-  // OPTIONS
+  // Options
   const ALLOW_COMMENTS: bool = false;
-
-  // READ MD FILE
+  // Read MD file
   let mut html: String = read_file("./test/basic/blockquote.md");
-
-  // PARSER METHODS
+  // Parser methods
   parser::escape::default(&mut html);
   parser::comments::default(&mut html, ALLOW_COMMENTS);
   parser::blockquotes::default(&mut html);
   parser::inline_code::default(&mut html);
   parser::emphasis::default(&mut html);
   parser::headings::default(&mut html);
-
-  // DEBUG
+  // Debug
   println!("{html}");
 }
