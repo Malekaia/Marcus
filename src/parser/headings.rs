2e029a9ce6f3e5071986a1f73437600ecb9badcf
Malekai <>
2022-10-04 16:04:35 +0100

    fix: output whitespace for headings

--- src/parser/headings.rs
+++ src/parser/headings.rs
@@ -6,13 +6,21 @@ pub fn default(html: &mut String) {
   // Headings (with ID), extract the heading's size, text and id
   replacer(html, RE::HEADING_WITH_ID, | capture: Captures |
     format!(
-      "<h{size} id=\"{id}\">{text}</h{size}>\n",
-      size = capture[2].trim().len(), text = capture[3].trim(), id = capture[4].trim()
+      "{prefix}<h{size} id=\"{id}\">{text}</h{size}>\n",
+      prefix = &capture[1],
+      size = capture[2].trim().len(),
+      text = capture[3].trim(),
+      id = capture[4].trim()
     )
   );
 
   // Headings (without ID), extract the heading's size and text
   replacer(html, RE::HEADING, | capture: Captures |
-    format!("<h{size}>{text}</h{size}>\n", size = capture[2].trim().len(), text = capture[3].trim())
+    format!(
+      "{prefix}<h{size}>{text}</h{size}>\n",
+      prefix = &capture[1],
+      size = capture[2].trim().len(),
+      text = capture[3].trim()
+    )
   );
 }
