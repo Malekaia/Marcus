69050759dda771fdcd2af09577027d2c335c518d
Malekai <>
2022-10-04 22:44:50 +0100

    Prevent parsing of empty emphasis captures

--- src/parser/emphasis.rs
+++ src/parser/emphasis.rs
@@ -2,23 +2,42 @@ use crate::regex::RE;
 use crate::parser::replacer;
 use regex::Captures;
 
-fn index(value: &str) -> usize {
-  if value.starts_with("*") { 1 } else {  2 }
+// Get the capture index for the emphasis ("*" = 1, "_" = 2)
+fn capture_index(value: &str) -> usize {
+  if value.starts_with("*") {
+    1
+  } else if value.starts_with("_") {
+    2
+  } else {
+    panic!("Marcus: ParseError: Emphasis match doesn't start with \"*\" or \"_\".")
+  }
+}
+
+// Ignore emphasis matches with no text
+fn capture_or_html(text: &str, capture: &str, html: String) -> String {
+  if text.len() < 1 {
+    capture.to_string()
+  } else {
+    html
+  }
 }
 
 pub fn default(html: &mut String) {
   // Bold Italic Emphasis
-  replacer(html, RE::EMPHASIS_BOLD_ITALIC, | capture: Captures |
-    format!("<b><i>{}</i></b>", &capture[index(&capture[0])].trim())
-  );
+  replacer(html, RE::EMPHASIS_BOLD_ITALIC, | capture: Captures | {
+    let text: &str = capture[capture_index(&capture[0])].trim();
+    capture_or_html(&text, &capture[0], format!("<b><i>{}</i></b>", text))
+  });
 
   // Bold Emphasis
-  replacer(html, RE::EMPHASIS_BOLD, | capture: Captures |
-    format!("<b>{}</b>", &capture[index(&capture[0])].trim())
-  );
+  replacer(html, RE::EMPHASIS_BOLD, | capture: Captures | {
+    let text: &str = capture[capture_index(&capture[0])].trim();
+    capture_or_html(&text, &capture[0], format!("<b>{}</b>", text))
+  });
 
   // Italic Emphasis
-  replacer(html, RE::EMPHASIS_ITALIC, | capture: Captures |
-    format!("<i>{}</i>", &capture[index(&capture[0])].trim())
-  );
+  replacer(html, RE::EMPHASIS_ITALIC, | capture: Captures | {
+    let text: &str = capture[capture_index(&capture[0])].trim();
+    capture_or_html(&text, &capture[0], format!("<i>{}</i>", text))
+  });
 }
