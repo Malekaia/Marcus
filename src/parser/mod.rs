aa2d67a121488d929a46a3ed240d024119612acc
Malekai <>
2022-10-08 03:38:05 +0100

    Avoid `Vec` collection

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -6,27 +6,24 @@ pub mod emphasis;
 pub mod headings;
 pub mod horizontal_rule;
 pub mod links_images_footnotes;
+pub mod lists;
 
 use regex::{Captures, Regex};
 
+// TODO: predefine regex
 pub fn replacer<F: FnMut(Captures) -> String>(html: &mut String, regex: &str, mut handler: F) {
   // Create the specified regular expression
   let re: Regex = Regex::new(regex).unwrap();
-
   // Ignore non-matches
-  if !re.is_match(&html) {
-    return ();
-  }
-
-  // Parse MD to HTML using user defined handler
-  let parsed: Vec<(String, String)> = re.captures_iter(&html)
-    // Replace captures with return of handler function
-    .map(| capture: Captures | (capture[0].to_string(), handler(capture)))
-    // collect into vector of string tuple
-    .collect::<Vec<(String, String)>>();
-
-  // Replace the MD capture with the parsed HTML
-  for (capture, parsed) in parsed {
-    *html = html.replace(&capture, &parsed);
+  if re.is_match(&html) {
+    // Insert full capture and parsed HTML into the result vector
+    let mut result: Vec<(String, String)> = vec![];
+    for capture in re.captures_iter(&html) {
+      result.push((capture[0].to_string(), handler(capture)));
+    }
+    // Replace the MD capture with the parsed HTML
+    for (capture, parsed) in result {
+      *html = html.replace(&capture, &parsed);
+    }
   }
 }
