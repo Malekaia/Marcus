012bede75506774e709bde7068303905fd173a4a
Malekai <>
2022-10-10 05:33:44 +0100

    Only remove three dash comments

--- src/parser/comments.rs
+++ src/parser/comments.rs
@@ -2,9 +2,9 @@ use crate::core::re;
 use regex::{Captures, Regex};
 
 // Parse: Optional Comments
-pub fn default(html: &mut String, allow_comments: bool) {
+pub fn default(html: &mut String) {
   let re_comments: Regex = re::from(re::COMMENTS);
-  re::parse(html, re_comments, | capture: Captures |
-    if allow_comments == false { String::new() } else { capture[0].to_string() }
+  re::parse(html, re_comments, | _: Captures |
+    String::new()
   );
 }
